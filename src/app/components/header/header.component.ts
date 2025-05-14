e8b35f8ec844fecad74234e0e65398056e5c58ba
Ivan Bazhenov <bazhenov.dev@yandex.ru>
2025-05-14 13:44:19 +0300

    fix header menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {socialData} from "../../utils/social-data";
 import {SocialDataType} from "../../types/social-data.type";
 
@@ -11,8 +11,16 @@ export class HeaderComponent {
   public socialData: SocialDataType = socialData;
 
   isActiveMenu: boolean = false;
+  isMobile: boolean = window.innerWidth <= 480;
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.isMobile = window.innerWidth <= 480;
+  }
 
   checkMenu(): void {
-    this.isActiveMenu = !this.isActiveMenu;
+    if (this.isMobile) {
+      this.isActiveMenu = !this.isActiveMenu;
+    }
   }
 }
